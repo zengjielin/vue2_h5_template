08aba7ed06f8ed0355111163e4f030cd0d1feb52
zengjielin <248717607@qq.com>
2020-05-22 16:38:55 +0800

    移除多余文件

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,23 @@ export default new Router({
     },
     {
       path: '/index',
-      name: 'Index',
+      name: 'index', //使用遵循横线连接 (kebab-case)
       meta: {
-        arrowText: "",
+        arrowText: "",//是否显示导航栏左箭头
         title: "主页",
         index: 1
       },
-      component: () => import("@/views/Index"),
+      component: () => import("@/views/index"),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      meta: {
+        arrowText: "",
+        title: "登录",
+        index: 1
+      },
+      component: () => import("@/views/login"),
     }
   ]
 })
